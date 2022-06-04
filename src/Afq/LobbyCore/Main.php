af087ebe96dd2dd047f11176b55dd673c97ce318
Afq <106860739+Adrianfq@users.noreply.github.com>
2022-06-04 22:09:24 +0800

    Update Main.php

--- src/Afq/LobbyCore/Main.php
+++ src/Afq/LobbyCore/Main.php
@@ -39,7 +39,7 @@ class Main extends PluginBase implements Listener {
 					"x" => 0.5,
 					"y" => 5,
 					"z" => 0.5,
-					"world" => "spawn",
+					"world" => "world",
 				]
             ]));
         } else {
